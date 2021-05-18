1a6595c4174115e7986354e33fe588c1b3af2732
h4sh3d <h4sh3d@protonmail.com>
2021-05-18 11:26:16 +0200

    Forbid unsafe code

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,8 @@
 //! [0]: https://github.com/monero-project/monero/blob/0a1ddc2eff854f3e932203a95b65a9f1efd60eef/contrib/epee/include/storages/portable_storage_from_bin.h
 //! [1]: https://github.com/monero-project/monero/blob/0a1ddc2eff854f3e932203a95b65a9f1efd60eef/contrib/epee/include/storages/portable_storage_to_bin.h
 
+#![forbid(unsafe_code)]
+
 mod de;
 mod error;
 mod ser;
